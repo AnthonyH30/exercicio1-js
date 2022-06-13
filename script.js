e1722f6e6963e53c7e97fdfccc4e98c95ffb1a0e
Anthony <anthonyharisson30@gmail.com>
2022-06-13 17:32:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let nome = 'anthony';
+
+let numero = 21;
+
+let ano = 2022;
+
+console.log(ano - numero);
+
+console.log("estou mexendo no console.log");
+
+let quartafeira = true;
+
+console.log(quartafeira);
+
+console.log(typeof(quartafeira))
